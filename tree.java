fa513d09054d5a4ed3ddc138729f3e3459c48f85
Rishi2117 <102339687+Rishi2117@users.noreply.github.com>
2022-05-12 12:25:11 +0530

    Add files via upload

---
+++ tree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class tree {
+    public static void main(String[] args) {
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        treeSet.add("B");
+        treeSet.add("A");
+        treeSet.add("C");
+        treeSet.add("C");
+
+        System.out.println(treeSet);
+    }
+}
