47bd03ea1c3da06d01bbd86061f745bf0da68fe2
Rishi2117 <102339687+Rishi2117@users.noreply.github.com>
2022-05-12 11:58:45 +0530

    Add files via upload

---
+++ arraydemo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+ class arraydemo
+ {
+ 	public static void main(String[] args) {
+ 		int [] anArray;
+ 		anArray = new int[10];
+ 		anArray [0] = 100;
+ 		anArray [1] = 200;
+ 		anArray [2] = 300;
+ 		anArray [3] = 400;
+ 		anArray [4] = 500;
+ 		anArray [5] = 600;
+ 		System.out.println("element at 0:"+anArray[0]);
+ 		System.out.println("element at 1:"+anArray[1]);
+ 		System.out.println("element at 2:"+anArray[2]);
+ 		System.out.println("element at 3:"+anArray[3]);
+ 		System.out.println("element at 4:"+anArray[4]);
+ 		System.out.println("element at 5:"+anArray[5]);
+
+ 	}
+ }
