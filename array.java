469e022b07eccb90fc14b50eb6b3077a9eaef6c9
Rishi2117 <102339687+Rishi2117@users.noreply.github.com>
2022-05-12 11:51:01 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class arra
+{
+	public static void main(String args[])
+	{
+		String str = "test";
+		System.out.println("String is :"+str);
+
+			String str1 = new String("test");
+		System.out.println("another is :"+str1);
+
+		int arr[];
+		arr =new int[2];
+		arr[0]=0;
+		arr[1]= 1;
+		System.out.println(arr);
+		System.out.println(arr[0]);
+	}
+}
